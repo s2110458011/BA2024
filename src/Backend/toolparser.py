f8e21aa35b3ddc833ffb42d5e2c2ed55ed571699
s2110458011 <s2110458011@fhooe.at>
2024-03-20 23:18:27 +0100

    Update toolparser.py

--- src/Backend/toolparser.py
+++ src/Backend/toolparser.py
@@ -10,23 +10,39 @@ def load_data_from_csv(file: str) -> pd.DataFrame:
     Returns:
         pd.DataFrame: Dataframe with survey data. Columns headers are the questions
     """
+
     df = pd.read_csv(file, header=0)
+    df = df.drop(columns='Zeitstempel')
     return df
 
 def extract_features(df: pd.DataFrame) -> np.array:
+    """_summary_
+
+    Args:
+        df (pd.DataFrame): Dataframe with survey data, headers are the questions.
+
+    Returns:
+        np.array: _description_
+    """
+
     features = []
     for column in df:
-        if column == 'Zeitstempel':
-            continue
         features.append(column)
     features = np.asarray(features)
     return features
 
 def extract_possible_answers(df: pd.DataFrame) -> dict:
+    """Extract unique (possible) answers from survey data for all questions.
+
+    Args:
+        df (pd.DataFrame): Dataframe with survey data, headers are the questions.
+
+    Returns:
+        dict: Questions: Unique answers
+    """
+
     answers = {}
     for column in df:
-        if column == 'Zeitstempel':
-            continue
         answers[column] = df[column].unique()
     return answers
 
