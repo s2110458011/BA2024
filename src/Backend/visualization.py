e148a8fa04a0264f3a7bec7167bbf0f9f908ffac
theresagschweidl <s2110458011@fhooe.at>
2024-03-23 13:56:35 +0100

    Create visualization.py

---
+++ src/Backend/visualization.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot
+
+from chart_data import ChartData
+
+"""Script for visualizing charts.
+"""
+
+def create_chart(data: pd.DataFrame, params: ChartData):
+    pass
+
+
