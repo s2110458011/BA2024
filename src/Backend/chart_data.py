9c258bf27e218f2647fe9905190f81203b766afc
theresagschweidl <s2110458011@fhooe.at>
2024-03-23 14:32:56 +0100

    Update chart_data.py

--- src/Backend/chart_data.py
+++ src/Backend/chart_data.py
@@ -1,5 +1,21 @@
+from typing import Union
 
 class ChartData():
 
-    def __init__(self, kind: str):
+    def __init__(self, kind: str = None, 
+                 title: str = None, 
+                 legend: Union[bool, str] = True) -> None:
         self.kind = kind
+        self.title = title
+        self.legend = legend
+
+    def set_kind(self, kind: str):
+        self.kind = kind
+    
+    def set_title(self, title: str):
+        self.title = title
+    
+    def set_legent(self, legend: Union[bool, str]):
+        self.legend = legend
+
+    
