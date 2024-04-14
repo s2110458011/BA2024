05d4979962363726cbf7cc162b153a0b5e38c07f
theresagschweidl <s2110458011@fhooe.at>
2024-04-14 13:27:50 +0200

    Update app.py

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,11 +1,40 @@
 from tkinter import *
+import customtkinter
+from homepage import *
+from load import *
 
-root = Tk()
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("green")
 
-root.title('Tkinter.com - Custom Tkinter!')
-#root.iconbitmap()
-root.geometry('600x350')
+class App(customtkinter.CTk):
+    def __init__(self, title, size):
+        
+        # main setup
+        super().__init__()
+        self.title(title)
+        self.geometry(f'{size[0]}x{size[1]}')
+        
+        self.menu = Menu(master=self, width=200)
+        self.container = customtkinter.CTkFrame(self, width=800)
+        
+        self.frames = {}
+        for F in (HomePage, Load):
+            frame = F(self.container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=1, sticky='nsew')
+        self.show_frame(HomePage)
+        
+        # run
+        self.mainloop()
+    
+    def create_layout(self):
+        self.menu.grid(column=0, sticky='ns')
+        self.container.grid(column=1, sticky='nsew')
+    
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
 
 
-
-root.mainloop()
+if __name__ == "__main__":
+    App('Survey Analzing', (1000,600))
