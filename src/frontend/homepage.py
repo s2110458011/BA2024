08c7d3a67a65df33fbcd1838fe0cdaef01760627
theresagschweidl <s2110458011@fhooe.at>
2024-04-14 14:36:19 +0200

    Update homepage.py

--- src/frontend/homepage.py
+++ src/frontend/homepage.py
@@ -2,7 +2,7 @@ from tkinter import *
 import customtkinter
 
 class HomePage(customtkinter.CTkFrame):
-    def __init__(self, master, **kwargs):
+    def __init__(self, master, controller, **kwargs):
         super().__init__(master, **kwargs)
-        
+        self.controller = controller
         
