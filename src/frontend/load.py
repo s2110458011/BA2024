f99697f9f01761466aef0be297d6605a43596a8f
theresagschweidl <s2110458011@fhooe.at>
2024-04-14 13:27:59 +0200

    Create load.py

---
+++ src/frontend/load.py
@@ -0,0 +1,31 @@
+from tkinter import *
+import customtkinter
+
+class Load(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        #self.grid(row=0, column=0, sticky='nsew')
+        
+        self.create_widgets()
+        self.create_layout()
+        
+    def create_widgets(self):
+        self.openfile = self.filedialog_frame()
+    
+    def create_layout(self):
+        self.openfile.grid(row=0, column=0, pady=(10,0), padx=(10,0))
+    
+    def filedialog_frame(self):
+        frame_openfile = customtkinter.CTkFrame(self, corner_radius=0)
+        # filename label
+        self.lbl_filename = customtkinter.CTkLabel(frame_openfile, text='Choose File', bg_color='grey', width=700)
+        self.lbl_filename.grid(row=0, column=0, padx=(10,10), pady=(10,10))
+        
+        # button open file dialog
+        btn_open = customtkinter.CTkButton(frame_openfile, text='Open', width=80, corner_radius=0, command=self.selectfile)
+        btn_open.grid(row=0, column=1, padx=(0,10), pady=(10,10))
+        return frame_openfile
+    
+    def selectfile(self):
+        filename = customtkinter.filedialog.askopenfilename()
+        self.lbl_filename.configure(text=filename, anchor='w', padx=5)
